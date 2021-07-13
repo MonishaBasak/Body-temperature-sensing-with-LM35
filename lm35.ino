f7f7fecd0e80f8ce2be8f2c6df853a06a25d8152
Monisha Basak <74670629+MonishaBasak@users.noreply.github.com>
2021-07-13 13:18:53 +0600

    Add files via upload

---
+++ lm35.ino
@@ -0,0 +1,14 @@
+#define LM35 A0
+
+void setup() {
+ Serial.begin (9600); // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ float lmvalue = analogRead(LM35);
+ float tempr = (lmvalue * 0.48825) ;
+ Serial.println(tempr); 
+ delay(1000);
+}
